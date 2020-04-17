69e27cdcf6820898d1db10ded241e6fe667278d0
Arun Kumar Selvaraj <dev@ioak.org>
2020-04-17 18:18:50 +0530

    Updates

---
+++ precache-manifest.b769fa334c1b18ecc59da94267a544a3.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97bfb6aeee449eb431bf3e440f7184f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d003d036f3f2cb13d402",
+    "url": "/static/css/2.cbf4de94.chunk.css"
+  },
+  {
+    "revision": "bc9ca78e58a0095d2888",
+    "url": "/static/css/main.3ffb5616.chunk.css"
+  },
+  {
+    "revision": "d003d036f3f2cb13d402",
+    "url": "/static/js/2.2cbb2ffe.chunk.js"
+  },
+  {
+    "revision": "8fa41d68784de15ba4dcfb6dcbc5f765",
+    "url": "/static/js/2.2cbb2ffe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc9ca78e58a0095d2888",
+    "url": "/static/js/main.4ffec257.chunk.js"
+  },
+  {
+    "revision": "9f51dc09dc138cb72b53",
+    "url": "/static/js/runtime-main.2383d372.js"
+  },
+  {
+    "revision": "85801247f8be07d222a3afe49d5d3b4c",
+    "url": "/static/media/github_black.85801247.svg"
+  },
+  {
+    "revision": "05f8f4246915c6020ee84c373e66c5a9",
+    "url": "/static/media/github_white.05f8f424.svg"
+  },
+  {
+    "revision": "6ff0d615d86c35cb0e61ad1a90622481",
+    "url": "/static/media/intro.6ff0d615.jpg"
+  },
+  {
+    "revision": "c077c1a3d70e8d9291e1b71306d9857a",
+    "url": "/static/media/ioak_black.c077c1a3.svg"
+  },
+  {
+    "revision": "5d65b00bb12411618929f81953c003eb",
+    "url": "/static/media/ioak_white.5d65b00b.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
