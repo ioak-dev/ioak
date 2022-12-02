a8d4f87ccaa778b50acce628de36b94f9ad5bc4d
Arunkumar Selvaraj <arun@ioak.io>
2022-12-02 13:02:00 +0530

    Section title accent improvements

--- src/components/AppCountSection/index.tsx
+++ src/components/AppCountSection/index.tsx
@@ -8,16 +8,10 @@ export default function AppCountSection() {
   return (
     <div className="app-count-section column-layout">
       <div className="app-count-section__heading">
-        <OakAnimateElement direction="none">
-          <h2 className="app-count-section__heading__title heading--2">Active Projects</h2>
-        </OakAnimateElement>
-        <OakAnimateElement direction="left">
-          <div className="app-count-section__heading__accent">
-            <div />
-            <div />
-            <div />
-          </div>
-        </OakAnimateElement>
+        <h2 className="app-count-section__heading__title heading--2">Active Projects</h2>
+        <div className="app-count-section__heading__accent">
+          <div />
+        </div>
       </div>
       <div className="app-count-section__container">
         {content.data.map(item => (
