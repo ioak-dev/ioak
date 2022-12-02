914cdf7bc3df08bd952ed127be2a006dd8d59c4d
Arunkumar Selvaraj <arun@ioak.io>
2022-12-02 17:14:20 +0530

    Implementation of section about Fortuna application

--- src/components/ProjectVariantOne/index.tsx
+++ src/components/ProjectVariantOne/index.tsx
@@ -17,34 +17,57 @@ interface Props {
 export default function ProjectVariantOne(props: Props) {
   return (
     <div className="project-variant-one column-layout">
-      <div className="project-variant-one__heading">
+      {/* <div className="project-variant-one__heading">
         <h3 className="project-variant-one__heading__title heading--3">{props.data.title}</h3>
         <div className="project-variant-one__heading__accent">
           <div />
         </div>
-      </div>
+      </div> */}
       <div className="project-variant-one__container">
         <div className="project-variant-one__container__first">
-          <div className="project-variant-one__container__first__left">
-            <div className="project-variant-one__container__first__left__top">
-              <div className="project-variant-one__container__first__left__top__tile-one">
-              {props.data.title}
+          <OakAnimateElement direction="right">
+            <div className="project-variant-one__container__first__right">
+              <img className="app-screenshot" src={props.data.screenshotOne} />
+            </div>
+          </OakAnimateElement>
+          <OakAnimateElement direction="left">
+            <div className="project-variant-one__container__first__left">
+              <div className="project-variant-one__container__first__left__top">
+                <div className="project-variant-one__container__first__left__top__tile-one">
+                  {props.data.title}
+                </div>
+                <div className="project-variant-one__container__first__left__top__tile-two">
+                  <img className="app-logo" src={props.data.logo} />
+                </div>
               </div>
-              <div className="project-variant-one__container__first__left__top__tile-two">
-              <img className="app-logo" src={props.data.logo} />
+              <div className="project-variant-one__container__first__left__bottom">
+                {props.data.descriptionOne}
               </div>
             </div>
-            <div className="project-variant-one__container__first__left__bottom">
-            {props.data.descriptionOne}
-              </div>
-          </div>
-          <div className="project-variant-one__container__first__right">
-            <img className="app-screenshot" src={props.data.screenshotOne} />
-          </div>
+          </OakAnimateElement>
         </div>
         <div className="project-variant-one__container__second">
-          <div>left</div>
-          <div>right</div>
+          <OakAnimateElement direction="left">
+            <div className="project-variant-one__container__second__right">
+              <div className="project-variant-one__container__second__right__tile-one">
+                {props.data.title}
+              </div>
+              <div className="project-variant-one__container__second__right__tile-two">
+                {props.data.title}
+              </div>
+              <div className="project-variant-one__container__second__right__tile-three">
+                {props.data.title}
+              </div>
+              <div className="project-variant-one__container__second__right__tile-four">
+                {props.data.title}
+              </div>
+            </div>
+          </OakAnimateElement>
+          <OakAnimateElement direction="right">
+            <div className="project-variant-one__container__second__left">
+              <img className="app-screenshot" src={props.data.screenshotOne} />
+            </div>
+          </OakAnimateElement>
         </div>
       </div>
     </div>
