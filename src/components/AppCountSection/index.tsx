6a1dbe341557b22dfc24369cb0109b22bd572720
Arunkumar Selvaraj <arun@ioak.io>
2022-12-05 19:24:31 +0530

    App statistics section layout change

--- src/components/AppCountSection/index.tsx
+++ src/components/AppCountSection/index.tsx
@@ -7,14 +7,11 @@ import OakAnimateElement from "../oakfly/OakAnimateElement"
 export default function AppCountSection() {
   return (
     <div className="app-count-section column-layout">
-      <div className="app-count-section__heading">
-        <h3 className="app-count-section__heading__title heading--3">Active Projects</h3>
-        <div className="app-count-section__heading__accent">
-          <div />
-        </div>
+      <div className="app-count-section__left">
+        {content.data.title} <span className="deemphasize-text">{content.data.subtitle}</span>
       </div>
-      <div className="app-count-section__container">
-        {content.data.map(item => (
+      <div className="app-count-section__right">
+        {content.data.tiles.map(item => (
           <SectionTile data={item} key={item.title} />
         ))}
       </div>
