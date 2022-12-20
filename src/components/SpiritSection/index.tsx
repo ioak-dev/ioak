bb02dae98cc2a861297746788cba251331542565
Arunkumar Selvaraj <arun@ioak.io>
2022-12-20 11:37:44 +0530

    Spirit section implementation

--- src/components/SpiritSection/index.tsx
+++ src/components/SpiritSection/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useRef, useState } from "react"
 import "./style.scss"
 import OakAnimateElement from "../oakfly/OakAnimateElement"
 
@@ -7,26 +7,27 @@ interface Props {
 }
 
 export default function SpiritSection(props: Props) {
-  const [messages, setMessages] = useState(["Innovation", "Open-mindedness", "Ascending", "Knowledge"]);
+  const [messages, setMessages] = useState(["Imaginative", "Optimistic", "Altruistic", "Knowledge Centric"]);
   const [currentIndex, setCurrentIndex] = useState(0);
+  const _currentIndex = useRef(0);
 
   useEffect(() => {
 
     setInterval(() => {
-      if (currentIndex < messages.length - 1) {
-        setCurrentIndex(currentIndex + 1);
+      if (_currentIndex.current < messages.length - 1) {
+        setCurrentIndex(_currentIndex.current + 1);
+        _currentIndex.current = _currentIndex.current + 1;
       } else {
         setCurrentIndex(0);
+        _currentIndex.current = 0;
       }
-    }, 3000);
+    }, 5000);
   }, [])
 
   return (
     <div className="spirit-section column-layout">
-      <div className="spirit-section__typewriter">
-        <div className="spirit-section__typewriter__text">
-          {messages[currentIndex]}
-        </div>
+      <div className="spirit-section__text">
+        {messages[currentIndex]}
       </div>
     </div>
   )
